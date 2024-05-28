460525fee4bec091414f52ef1a63d9109afe266b
Ahmed Elhadi <ahmed.e_ali@outlook.com>
2024-05-28 14:44:14 +0400

    website created
    website created

---
+++ hassabsoft/script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Your message has been sent!');
+    // Here you can add code to actually send the form data to your email or server
+});
